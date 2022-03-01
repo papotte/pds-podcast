25a1c088fcc5735e77c29b59660c12d2b5f1e7c3
Rossana Bermudez <rossana.bermudez@i2x.ai>
2022-03-01 20:17:55 +0100

    Update pds-podcast.php

--- pds-podcast.php
+++ pds-podcast.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: PdS Podcast
- * Version: 1.0.0
+ * Version: 1.0.1
  * Plugin URI: https://github.com/papotte/pds-podcast
  * Description: Based on Seriously Simple Podcasing
  * Author: papotte
